a2d94d8ef15bf46c5de5d68d9c33541bc743cbd4
Cynthia Fan <cynthiafan1230@gmail.com>
2020-05-20 16:46:26 +0800

    fix: [storybook] add storybook ga id

--- config/storybook/manager.js
+++ config/storybook/manager.js
@@ -1,6 +1,8 @@
 import { addons } from '@storybook/addons';
 import portfolio from './portfolio';
 
+window.STORYBOOK_GA_ID = 'UA-91843640-4';
+
 addons.setConfig({
   theme: portfolio,
   panelPosition: 'right',
