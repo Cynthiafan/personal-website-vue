9e996d1918e3fbf23cc472c267e7a3a503bc5ce3
Cynthia Fan <cynthiafan1230@gmail.com>
2020-06-24 18:06:16 +0800

    feat: [homepage] add vue-analytics
    - remove storybook addon ga

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import { BootstrapVue } from 'bootstrap-vue';
+import VueAnalytics from 'vue-analytics';
 
+import { BootstrapVue } from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
@@ -14,6 +15,13 @@ import i18n from './plugins/i18n';
 import '@/mixins';
 
 library.add(faGithub, faLinkedinIn, faEnvelope, faQuoteRight, faVolleyballBall, faPuzzlePiece, faFilm, faGlobe);
+
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-91843640-4',
+    router,
+  });
+}
 
 Vue.use(BootstrapVue);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
